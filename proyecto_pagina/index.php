b6b9e10ba7e4e1eb8276864b4a08be310515cb8d
EdsonPrado16 <edsonmoisesprado23@cbtis52.edu.mx>
2025-10-29 16:56:28 -0600

    Add files via upload

---
+++ proyecto_pagina/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8" />
+  <title>Directorio - Inicio</title>
+  <link rel="stylesheet" href="estilo.css">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+</head>
+<header>
+  <link rel="stylesheet" href="estilo.css">
+  <h1>🚨 Directorio | Servicios de Emergencia</h1>
+  <nav>
+    <a href="index.php">Inicio</a>
+    <a href="registrar.php">Registrar</a>
+    <a href="iniciar_sesion.php">Iniciar sesión</a>
+  </nav>
+</header>
+  <button class="boton-911">911</button>
+
+<section class="hero">
+  <h2>Bienvenido</h2>
+  <p>Accede a contactos de emergencia y administra usuarios de forma segura y rápida.</p>
+  <div class="dropdown-side">
+  <button id="btnEmergencia">🚨 Reportar emergencia</button>
+  <div class="dropdown-side-content">
+    <a href="emergencia medica.html">🚑 Emergencia médica</a>
+    <a href="bomberos.html">🚒 Bomberos</a>
+    <a href="policia.html">🚓 Policía</a>
+    <a href="fuga de gas o electricidad.html">⚡ Fuga de gas / electricidad</a>
+    <a href="Desaparicion o violencia.html">🧍 Desaparición o violencia</a>
+    <a href="otros servicios.html">☎ Otros servicios</a>
+  </div>
+</div>
+
+</section>
+
+
+<footer>
+  <p>
+    © 2025 — Proyecto de programación. Hecho por Isaak y Edson.</p>
+</footer>
